bb61e1b73650e01e63caa3fa6d0521da270e41f1
oschucht <oschucht@tecracer.de>
2017-01-08 17:26:09 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 });
