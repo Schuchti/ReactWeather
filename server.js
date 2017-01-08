4d57dd4c9161c97702227d381f431fee0cf5d6a0
oschucht <oschucht@tecracer.de>
2017-01-08 17:32:46 +0100

    Prep repo for Heroku again

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(function(request, response, next) {
+  if (request.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    response.redirect('http://' + request.hostname + request.url)
+  }
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
