059a21f0b31c68a17552788c7a9113da02b67b45
oschucht <oschucht@tecracer.de>
2017-01-10 16:31:52 +0100

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(request, response, next) {
-  if (request.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (request.headers['x-forwarded-proto'] === 'https') {
     response.redirect('http://' + request.hostname + request.url)
+  } else {
+    next();
   }
 });
 
